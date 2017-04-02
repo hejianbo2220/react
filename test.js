4ca010e8e0d439a6ce192e5623f3bad2fee4bf77
何剑波 <112526920@qq.com>
2017-04-02 16:53:20 +0800

    no message

--- test.js
+++ test.js
@@ -1,2 +1,27 @@
-
-ReactDOM.render(<FilterableProductTable products={products} />, document.getElementById("app"));
+class TodoList extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			todo: "",
+			todos: []
+		}
+		this.addTodo = this.addTodo.bind(this);
+	}
+	addTodo (e) {
+		const {todo, todos} = this.state;
+		todos.push(todo);
+		this.setState({todo: "", todos});
+	}
+	render () {
+		return (
+			<div>
+				<h1>待办列表</h1>
+				<div>
+					<input type="text" onChange={e => this.setState({todo: e.target.value})} />
+					<button onClick={this.addTodo}>添加</button>
+				</div>
+			</div>
+		);
+	}
+}
+ReactDOM.render(<TodoList />, document.getElementById("app"));
